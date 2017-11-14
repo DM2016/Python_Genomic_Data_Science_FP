26e531aca9ff563506ffc2624aba2544c3c4c9c9
Diego Morales <Diego.Morales@rockets.utoledo.edu>
2017-11-13 21:18:51 -0500

    Update Fasta_sequence_counter_q1.py

--- Fasta_sequence_counter_q1.py
+++ Fasta_sequence_counter_q1.py
@@ -2,9 +2,9 @@
 import Bio
 from Bio import SeqIO
 with open('dna.example.fasta', 'r') as f:  #open the dna exmaple fasta file to read, change file names accordingly wherever they occur
-        seq_count = 0
-        for seq in SeqIO.parse('dna.example.fasta', "fasta"):
-                seq_count = seq_count + 1
-        print("There were " + str(seq_count) + " records in file " + "dna.example.fasta")
+        seq_count = 0 #set initial seq_count value at 0
+        for seq in SeqIO.parse('dna.example.fasta', "fasta"): #use for loop to call SeqIO function parse on fasta file
+                seq_count = seq_count + 1 #perform and keep track of seq_count
+        print("There were " + str(seq_count) + " records in file " + "dna.example.fasta") #print results
 
-f.close()
+f.close() #close program
